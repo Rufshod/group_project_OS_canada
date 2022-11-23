bfb528d15b9176d1ff4b614540b1f261d39258f7
Dorota Bjoorn <Dorota.Bjoorn@iths2022>
2022-11-23 13:22:11 +0100

    removed redundant file layout.py

--- layout.py
+++
@@ -1,9 +0,0 @@
-# Add code here for layout.
-
-from dash import html, dcc
-import dash_bootstrap_components as dbc
-
-
-class Layout:
-    def __init__(self, symbol_dict: dict) -> None:
-        self._symbol_dict = symbol_dict
